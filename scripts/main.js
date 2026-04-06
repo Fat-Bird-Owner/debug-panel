fc04fe33fb5782243d5379a4b9372c1c9a5396cc
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-06 09:28:32 +0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+Events.on(ContentInitEvent, () => {
+try{
+
+function setupUI() {
+    let overlaymarkerTable = Vars.ui.hudGroup.find("statustable");
+    overlaymarkerTable.row();
+
+    let tab = new Table();
+    overlaymarkerTable.add(tab).bottom().left();
+
+    tab.table(Tex.pane, t => {
+        let b = new Button(Styles.none);
+        let icon = new TextureRegionDrawable(Core.atlas.find("dbm-command-block"));
+        b.button(icon, () => {
+
+        });
+        t.add(b);
+
+        t.clicked(() => {
+
+        });
+    });
+
+    tab.visibility = () => {
+        // return (Vars.ui.hudfrag.shown && Vars.mobile && !Vars.net.client() ? true : false)
+        return (Vars.ui.hudfrag.shown && !Vars.net.client() ? true : false)
+    }
+}
+
+} catch(e){
+Vars.ui.showInfoToast(e,15);
+}});
