4992e317952d4e24e1328d858597fd84e75dede7
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-18 14:45:46 +0700

    Update command.js

--- scripts/command.js
+++ scripts/command.js
@@ -180,7 +180,7 @@ function panel(){
                 } catch(e){
                 Vars.ui.showInfoToast(e, 5);           
                 }});
-                }).size(Core.graphics.getWidth() / 2, Core.graphics.getHeight() / 2);
+                }).width(Core.graphics.getWidth()).growY();
                     //dialog.cont.pane({}).size(400,300);
                     dialog.addCloseButton();
                     dialog.show();
