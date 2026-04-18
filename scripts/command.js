9f6ee6d3c800c9c537251c14ecb4ea71d067e4fa
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-18 12:07:40 +0700

    Update command.js

--- scripts/command.js
+++ scripts/command.js
@@ -211,7 +211,7 @@ function panel(){
                     Sounds.uiButton.play();
                     if (consolePanel == null){
                     consolePanel = new BaseDialog("panel");
-                    consolePanel.cont.top().padTop(20);
+                    consolePanel.cont.top();
 
                     const field = new TextField("");
                     field.setMessageText("Insert Command");
