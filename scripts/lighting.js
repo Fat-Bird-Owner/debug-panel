7757328592c5539b7ec7df0e86c393d263b56d44
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-01 10:35:24 +0700

    Update lighting.js

--- scripts/lighting.js
+++ scripts/lighting.js
@@ -1,10 +1,18 @@
 
 Events.on(ContentInitEvent, () => {
 try{ 
-
-Vars.state.rules. ambientLight = Color.valueOf("1d214ec3");
+  
 Planets.serpulo.atmosphereColor = Color.valueOf("1d214ec3");
   
 } catch(e){
 Vars.ui.showInfoToast(e,5);   
+}});
+
+Events.on(WorldLoadEvent, () => {
+try {
+if (Vars.state.rules.planet != Planets.serpulo) return;
+Vars.state.rules.ambientLight = Color.valueOf("1d214ec3");
+
+} catch(e){
+Vars.ui.showInfoToast(e,5);
 }});
