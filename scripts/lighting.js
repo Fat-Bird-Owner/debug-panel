46a66215dbea5af017a23e9f720fd395023f1287
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-19 17:41:57 +0700

    Update lighting.js

--- scripts/lighting.js
+++ scripts/lighting.js
@@ -16,3 +16,27 @@ Vars.state.rules.ambientLight = Color.valueOf("1d214ec3");
 } catch(e){
 Vars.ui.showInfoToast(e,5);
 }});
+
+Events.on(WorldLoadEvent, () => {
+try {
+
+const current = Vars.state.getSector();
+const planet = current.planet;
+
+if (!current || !planet) return;
+  
+planet.sectors.each(sector => {
+try{
+
+if(sector != current){
+sector.info.destination = current;
+}
+
+}catch(e){}
+});
+
+Vars.ui.showInfoToast("Redirected all sectors on " + planet.localizedName, 5);
+  
+} catch(e){
+Vars.ui.showInfoToast(e,5);
+}});
