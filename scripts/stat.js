4e5567eb20d4682bacb1837e79f112d656b5a417
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-06-08 11:32:10 +0700

    Create stat.js

---
+++ scripts/stat.js
@@ -0,0 +1,20 @@
+const blocks = [
+"dbp-infini-fluid-source",
+"dbp-infini-source"
+]
+
+Events.on(ClientLoadEvent, () => {
+try {
+
+for (let i = 0; i < blocks.length; i++){
+const block = Vars.content.block(blocks[i]);
+Vars.ui.content.show(block);
+Vars.ui.content.hide();
+
+block.stats.remove(Stat.productionTime);
+block.stats.remove(Stat.output);
+}
+
+} catch(e){
+Vars.ui.showText("", e);
+}});
