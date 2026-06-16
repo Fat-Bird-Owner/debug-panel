8f80952876b92063a6eef9b06931b0253c47c39d
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-06-16 22:46:47 +0700

    Update Redirect.js

--- scripts/Redirect.js
+++ scripts/Redirect.js
@@ -1,6 +1,8 @@
 Events.on(WorldLoadEvent, () => {
 try {
 
+let exports = 0;
+  
 const current = Vars.state.getSector();
 if (!current || !current.planet) return;
   
@@ -11,12 +13,14 @@ try{
 
 if(sector != current){
 sector.info.destination = current;
+
+if (sector.info.export.size() != 0) exports++;
 }
 
 }catch(e){}
 });
 
-Vars.ui.showInfoToast("[lightgrey]Redirected all launch pads on [accent]" + planet.localizedName, 5);
+if (exports != 0) Vars.ui.showInfoToast("[lightgrey]Redirected all launch pads on [accent]" + planet.localizedName, 5);
   
 } catch(e){
 Vars.ui.showInfoToast(e,5);
